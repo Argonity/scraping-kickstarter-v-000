5c4698f062304b5ba93ae6e45500d6384756f6b3
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-07-07 01:08:41 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,7 +23,7 @@ def create_project_hash
 <<<<<<< HEAD
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 =======
-      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i      
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 >>>>>>> 8763c25eda4d00347e2d1ffa70b8d6fb51582116
     }
   end
